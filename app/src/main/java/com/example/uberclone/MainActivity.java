889598c57075e36b78dbc480c19e41536a553e2a
elsay <almasry2100@yahoo.com>
2020-07-21 02:21:02 +0400

    added App,Parse,Github

--- app/src/main/java/com/example/uberclone/MainActivity.java
+++ app/src/main/java/com/example/uberclone/MainActivity.java
@@ -4,11 +4,15 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+import com.parse.ParseInstallation;
 
+public class MainActivity extends AppCompatActivity {
+//added
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate( savedInstanceState );
         setContentView( R.layout.activity_main );
+        ParseInstallation.getCurrentInstallation().saveInBackground();
+
     }
 }
